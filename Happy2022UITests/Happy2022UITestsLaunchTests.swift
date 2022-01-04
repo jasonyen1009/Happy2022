efc74b8d87d096e3389b991f8e79e22fff8b8e2c
Jason <jasonyen1009@gmail.com>
2022-01-04 20:02:42 +0800

    Initial Commit

---
+++ Happy2022UITests/Happy2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Happy2022UITestsLaunchTests.swift
+//  Happy2022UITests
+//
+//  Created by Hong Cheng Yen on 2022/1/4.
+//
+
+import XCTest
+
+class Happy2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
